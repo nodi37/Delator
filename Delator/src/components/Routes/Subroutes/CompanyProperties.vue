504f89ad5244271425b81d823e067bd7dc73a5d7
nodi37 <nodi37@o2.pl>
2022-07-25 18:19:34 +0200

    Company properties route changes

--- Delator/src/components/Routes/Subroutes/CompanyProperties.vue
+++ Delator/src/components/Routes/Subroutes/CompanyProperties.vue
@@ -19,7 +19,6 @@ export default {
         addMode: false,
         tabs: [
             { name: 'public-info', icon: 'mdi-information-outline' },
-            { name: 'contracts', icon: 'mdi-file-sign' },
             { name: 'settings', icon: 'mdi-cog' },
         ],
     }),
@@ -108,55 +107,6 @@ export default {
                             </template>
                         </InfoBoard>
                     </v-tab-item>
-
-
-
-                    <v-tab-item>
-                        <AddContractForm v-if="addMode" v-on:addedContract="addedContract" />
-
-                        <v-expansion-panels v-else>
-
-                            <v-expansion-panel v-for="contract in companyContracts" :key="contract._id">
-
-                                <v-expansion-panel-header>
-                                    {{ $t('contract-with') + ': ' + contract.companyName }}
-                                </v-expansion-panel-header>
-
-                                <v-expansion-panel-content>
-                                    <InfoBoard :data="
-                                        [
-                                            { type: 'text', name: 'with', value: contract.companyName },
-                                            { type: 'text', name: 'contract-type', value: contract.contractType },
-                                            { type: 'percent', name: 'employment-dimension', value: contract.employmentDimension },
-                                            { type: 'text', name: 'hourly-wage', value: contract.hourlyWage },
-                                        ]
-                                    ">
-                                        <template v-slot:actions>
-                                            <v-btn color="#00E676" class="white--text mr-4" elevation="2" @click="downloadContract(contract._id)">
-                                                {{ $t('download') }}
-                                            </v-btn>
-
-                                            <v-btn color="primary" elevation="2" @click="editMode = !editMode">
-                                            {{ $t('edit') }}
-                                            </v-btn>
-
-                                        </template>
-                                    </InfoBoard>
-                                </v-expansion-panel-content>
-
-                            </v-expansion-panel>
-
-                        </v-expansion-panels>
-                        <div v-if="!addMode" class="d-flex justify-end">
-
-                            <v-btn color="#00E676" class="white--text ma-1 mt-4" elevation="2" @click="addMode = !addMode">
-                                {{ $t('add') }}
-                            </v-btn>
-
-                        </div>
-                    </v-tab-item>
-
-
 
                     <v-tab-item>
                         <EditCompanySettingsForm class="pa-4" v-if="editMode" v-on:edditedSettings="edditedSettings"
